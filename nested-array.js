f9b776fe08584ca63d92fc488e43bc4996616a56
yohanessahrul <yohanessahrul92@gmail.com>
2018-03-07 10:20:46 +0700

    nested-array

---
+++ nested-array.js
@@ -0,0 +1,16 @@
+function createNestedArr(jumlahRow, jumlagCol){
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+    let bigWrapArray = []
+    for(let i=0; i<jumlahRow; i++){
+        let colArray = []
+        for(let j=0; j<jumlagCol; j++){
+            colArray.push(alphabet.charAt(Math.floor(Math.random()*alphabet.length)))
+        }
+        bigWrapArray.push(colArray)
+    }
+    return bigWrapArray;
+}
+
+console.log(createNestedArr(5,3))
+console.log(createNestedArr(4,2))
