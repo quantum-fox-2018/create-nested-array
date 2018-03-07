18461454c46810337c7800fb73f76c76857ac732
davidjoshua87 <davidjosh_87@yahoo.com>
2018-03-07 11:19:39 +0700

    create-nested-array-revisi

--- createNastedArray.js
+++ createNastedArray.js
@@ -1,10 +1,10 @@
-function createNestedArray(jumlahRow, jumlahCol){
+function createNestedArray(jumlahRow, jumlahCol) {
   var hasil = [];
 
-  for(var i=0; i<jumlahRow; i++){
+  for (var i = 0; i < jumlahRow; i++) {
     var row = [];
-    for(var j=0; j<jumlahCol; j++){
-      var random = String.fromCharCode(97 + Math.ceil(Math.random()*26));
+    for (var j = 0; j < jumlahCol; j++) {
+      var random = String.fromCharCode(96 + Math.ceil(Math.random() * 26));
       row.push(random);
 
     }
@@ -13,6 +13,6 @@ function createNestedArray(jumlahRow, jumlahCol){
   return hasil;
 }
 
-console.log(createNestedArray(5,3));
+console.log(createNestedArray(5, 3));
 console.log('', '');
-console.log(createNestedArray(4,2));
+console.log(createNestedArray(4, 2));
