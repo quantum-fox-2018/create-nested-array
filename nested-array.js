401feffd25f6f83986f0f3a494a52cb1a213374b
barestu <fheartnet@gmail.com>
2018-03-07 10:50:33 +0700

    Soal Nested Array

---
+++ nested-array.js
@@ -0,0 +1,23 @@
+function  createNestedArr(jumlahRow, jumlahCol) {
+  var nestedArr = [];
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+  //console.log(randomAlphabet);
+
+  for (let i = 0; i < jumlahRow; i++) {
+    var newRow = [];
+    for (let j = 0; j < jumlahCol; j++) {
+      var randomAlphabet = alphabet.charAt(Math.floor(Math.random() * Math.floor(alphabet.length)));
+      newRow.push(randomAlphabet);
+    }
+    nestedArr.push(newRow);
+  }
+
+  return nestedArr;
+}
+
+console.log(createNestedArr(5,3));
+console.log('');
+console.log(createNestedArr(4,2));
+console.log('');
+console.log(createNestedArr(6,4));
